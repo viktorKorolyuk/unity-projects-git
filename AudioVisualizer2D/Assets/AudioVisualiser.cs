fceded353707fa905647e7397aa68da8478976cd
Viktor Korolyuk <viktor.korolyuk@gmail.com>
2017-04-29 21:27:11 -0400

    Testing FFT in Unity

---
+++ AudioVisualizer2D/Assets/AudioVisualiser.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioVisualiser : MonoBehaviour {
+
+	AudioSource audioSource;
+	// Use this for initialization
+	void Start () {
+		audioSource = GetComponent<AudioSource> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float[] spectrum = new float[256];
+
+		AudioListener.GetSpectrumData(spectrum, 0, FFTWindow.Rectangular);
+
+		Debug.Log (Mathf.Log(spectrum [0]));
+		for (int i = 0; i < spectrum.Length-1; i++) {
+			Debug.DrawLine(new Vector3(i - 10, spectrum[i], 0), new Vector3(i-9, spectrum[i + 1] + 10, 0), Color.red);
+			if (i + 1 != spectrum.Length)
+				spectrum [i + 1] += 10;
+		}
+	}
+}
--- Mario/MarioProject/Assets/Scripts/CameraController.cs
+++ Mario/MarioProject/Assets/Scripts/CameraController.cs
@@ -2,34 +2,40 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraController : MonoBehaviour {
-
-	public static float minX;
-	public static float width;
+public class CameraController : MonoBehaviour
+{
 	public Transform player;
 
-	float offset;
+	Vector3 properCameraPositon;
+	Vector3 playerScreenPos;
+
+	float offsetX;
+	float locationY;
 
 	void Start () {
-		offset = transform.position.x - player.position.x;
-
-		width = GetComponent<Camera> ().pixelWidth;
+		offsetX = transform.position.x - player.position.x;
 	}
 
+
 	void LateUpdate () {
-		minX = transform.position.x;
+		properCameraPositon = transform.position; //I moveed the properCameraPosition set to the update. Without resetting the value each time, it would accumulate
 
-		Vector3 playerPos = Camera.main.WorldToScreenPoint(player.position);
+		//get player position in the "real world" to a camera location
+		playerScreenPos = Camera.main.WorldToScreenPoint (player.position);
 
-		float yDif = 0;
-		if (playerPos.y > Screen.height/2) {
-			//start tracking him
-			yDif = playerPos.y - Screen.height/2;
-		} else if (playerPos.y < Screen.height/4) {
-			//start tracking him
-			yDif = playerPos.y - Screen.height/4;
+		if (playerScreenPos.y >= Screen.height / 2) { //player is at the correct height
+			//the camera position
+			properCameraPositon.y += playerScreenPos.y - (Screen.height / 2);
+		} else if (playerScreenPos.y <= (Screen.height / 10)) { //player is at correct height
+			//how low below the threshold is the player?
+			properCameraPositon.y += playerScreenPos.y - (Screen.height / 10); //this gives a negative number, so we don't need to subtract it
 		}
-		transform.position = new Vector3 (offset + player.position.x, Mathf.Lerp (transform.position.y, transform.position.y + yDif, Time.deltaTime), transform.position.z);
 
+		locationY = Mathf.Lerp (transform.position.y, properCameraPositon.y, Time.deltaTime);
+		print (locationY);
+
+		transform.position = new Vector3 (offsetX + player.position.x, //set camera position based on xOffset.
+			locationY, //set the interpolated Y value
+			transform.position.z); //set the z value, not sure why it was changing in the first place
 	}
 }
