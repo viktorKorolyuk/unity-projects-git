fa4c0fb9b86acaeaa876e6bf00440c806dc674b0
Viktor Korolyuk <viktor.korolyuk@gmail.com>
2017-05-21 20:02:21 -0400

    Working on Pong AI

---
+++ pong/Assets/scripts/paddleMove.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class paddleMove : MonoBehaviour {
+
+	public float speed = 10f;
+	// Update is called once per frame
+	void Update () {
+		float y = 0;
+		if (Input.GetAxis ("pongVertical") > 0) {
+			y = speed;
+		} else if(Input.GetAxis("pongVertical") < 0){
+			y = -speed;
+		}
+		transform.position += new Vector3 (0, y * Time.deltaTime, 0);
+	}
+}
